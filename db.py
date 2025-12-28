5cf48a581e0f8a42a67380ad6de16ea37616e88b
litcq <75427781+sheva2250@users.noreply.github.com>
2025-12-29 01:43:16 +0700

    Update db.py

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 _pool = None
 
+
 async def init_pool():
     """Initialize the connection pool on startup"""
     global _pool
@@ -16,24 +17,24 @@ async def init_pool():
             # KONFIGURASI AGRESIF UNTUK MENGHINDARI "GHOST CONNECTION"
             _pool = await asyncpg.create_pool(
                 DATABASE_URL,
-                min_size=1,         # Cukup 1 koneksi standby
-                max_size=5,         # Maksimal 5 biar gak rebutan
-                
+                min_size=1,  # Cukup 1 koneksi standby
+                max_size=10,  # Maksimal 5 biar gak rebutan
+
                 # TIMEOUT SETTINGS (PENTING!)
-                timeout=10,         # Waktu maks nunggu dapet slot koneksi (10s)
-                command_timeout=10, # Waktu maks nunggu query SQL selesai (10s) - JANGAN 60s!
-                
+                timeout=10,  # Waktu maks nunggu dapet slot koneksi (10s)
+                command_timeout=10,  # Waktu maks nunggu query SQL selesai (10s) - JANGAN 60s!
+
                 # CONN LIFETIME
-                max_inactive_connection_lifetime=300, # 5 menit idle = bunuh koneksi (biar fresh)
-                
+                max_inactive_connection_lifetime=300,  # 5 menit idle = bunuh koneksi (biar fresh)
+
                 # SSL
                 ssl="require",
-                
+
                 # TCP Keepalives (Biar koneksi gak diputus diem-diem sama firewall)
                 server_settings={
                     "application_name": "LyraBot",
                     # Paksa kirim sinyal 'ping' TCP tiap 10 detik
-                    "tcp_keepalives_idle": "10", 
+                    "tcp_keepalives_idle": "10",
                     "tcp_keepalives_interval": "5",
                     "tcp_keepalives_count": "3"
                 }
@@ -44,12 +45,14 @@ async def init_pool():
             # Biarkan _pool None, nanti error akan ditangkap di bot-main.py
     return _pool
 
+
 async def get_pool():
     """Get or create the connection pool"""
     global _pool
     if _pool is None:
         _pool = await init_pool()
     return _pool
+
 
 async def close_pool():
     """Close the connection pool gracefully"""
